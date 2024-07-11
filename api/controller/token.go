88049155c5299c5346b8adafb9c72cafeb7fd57c
Renan Machado <renanoliveira2199@gmail.com>
2024-07-11 11:43:09 -0300

    on renderapp

--- api/controller/token.go
+++ api/controller/token.go
@@ -72,8 +72,8 @@ func LoginHandler(c *gin.Context) {
 
 func DeleteCookie(c *gin.Context) {
 	c.SetSameSite(http.SameSiteNoneMode)
-	c.SetCookie("user", "", -1, "/", "banded-arcana-428116-d2.uc.r.appspot.com", true, true)
-	c.SetCookie("token", "", -1, "/", "banded-arcana-428116-d2.uc.r.appspot.com", true, true)
+	c.SetCookie("user", "", -1, "/", "carteirago.onrender.com", true, true)
+	c.SetCookie("token", "", -1, "/", "carteirago.onrender.com", true, true)
 	c.String(http.StatusOK, "log out")
 	c.Done()
 	//c.String(http.StatusOK, "Cookie has been deleted")
@@ -83,8 +83,8 @@ func setCookieHandler(c *gin.Context, email string, jwt string) {
 	c.SetSameSite(http.SameSiteNoneMode)
 	_, _, err := getCookieHandler(c)
 	if err != nil {
-		c.SetCookie("user", email, 3600, "/", "banded-arcana-428116-d2.uc.r.appspot.com", true, true)
-		c.SetCookie("token", jwt, 3600, "/", "banded-arcana-428116-d2.uc.r.appspot.com", true, true)
+		c.SetCookie("user", email, 3600, "/", "carteirago.onrender.com", true, true)
+		c.SetCookie("token", jwt, 3600, "/", "carteirago.onrender.com", true, true)
 	}
 }
 
